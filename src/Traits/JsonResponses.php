221e6c2ceb734f69520a3152b6218be4c404fd7e
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-05-01 12:49:11 +0100

    Updating JsonResponses Trait

--- src/Traits/JsonResponses.php
+++ src/Traits/JsonResponses.php
@@ -34,7 +34,7 @@ trait JsonResponses
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function jsonResponseSuccess(array $data, $status = 200, $code = 'success', array $headers = [], $options = 0)
+    public function jsonResponseSuccess(array $data = [], $status = 200, $code = 'success', array $headers = [], $options = 0)
     {
         return $this->jsonResponse()->success($data, $status, $code, $headers, $options);
     }
@@ -42,7 +42,7 @@ trait JsonResponses
     /**
      * Respond with an error response.
      *
-     * @param  string  $message
+     * @param  array   $data
      * @param  int     $status
      * @param  string  $code
      * @param  array   $headers
@@ -50,8 +50,8 @@ trait JsonResponses
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function jsonResponseError($message, $status = 400, $code = 'error', array $headers = [], $options = 0)
+    public function jsonResponseError(array $data = [], $status = 400, $code = 'error', array $headers = [], $options = 0)
     {
-        return $this->jsonResponse()->error($message, $status, $code, $headers, $options);
+        return $this->jsonResponse()->error($data, $status, $code, $headers, $options);
     }
 }
