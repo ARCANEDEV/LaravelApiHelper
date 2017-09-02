4aca67e30889b55d75bb936c7c547d14ee8d19b5
Unknown <arcanedev.maroc@gmail.com>
2017-09-02 13:04:19 +0100

    Minor fixes

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ Laravel API Helper is a package that allows you to add features to build api fas
 
 ## Table of contents
 
-  1. [Installation and Setup](_docs/1.Installation-and-Setup.md)
-  2. [Configuration](_docs/2.Configuration.md)
-  3. [Usage](_docs/3.Usage.md)
+  1. [Installation and Setup](_docs/1-Installation-and-Setup.md)
+  2. [Configuration](_docs/2-Configuration.md)
+  3. [Usage](_docs/3-Usage.md)
 
 ### Features
 
--- src/ApiHelperServiceProvider.php
+++ src/ApiHelperServiceProvider.php
@@ -14,6 +14,7 @@ class ApiHelperServiceProvider extends PackageServiceProvider
      |  Properties
      | -----------------------------------------------------------------
      */
+
     /**
      * Package name.
      *
@@ -25,6 +26,7 @@ class ApiHelperServiceProvider extends PackageServiceProvider
      |  Main Methods
      | -----------------------------------------------------------------
      */
+
     /**
      * Register the service provider.
      */
